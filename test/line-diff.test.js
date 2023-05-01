73c15920f70878d328351d745d9863dc2ae147ed
Jordan Pyott <jordanpyott@gmail.com>
2023-05-01 22:45:03 +1200

    GH-25: adding optional flags and post-options for line-diff.js (#26)
    * GH-25: adding optional flags and post-options for line-diff.js
    
    * Delete yarn.lock
    
    ---------
    
    Co-authored-by: fengmk2 <fengmk2@gmail.com>

--- test/line-diff.test.js
+++ test/line-diff.test.js
@@ -13,3 +13,14 @@ describe('./test/line-diff.test.js', () => {
     assert.equal(typeof res, 'object');
   });
 });
+
+describe('./test/line-diff.test.js with filters', () => {
+  let res;
+  it('should parse git', () => {
+    res = diff({
+      currentBranch: 'master',
+      filetypes: ["*.ts", "*.js"]
+    });
+    assert.equal(typeof res, 'object');
+  });
+});
